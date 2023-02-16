037932b075b10d4a91510f893914960e0815c114
TGGG <65331833+TheGoodGamerGuy@users.noreply.github.com>
2023-02-16 17:47:37 +0200

    changed something
    idk

--- src/index.js
+++ src/index.js
@@ -1,19 +1,37 @@
-var CoolButton = document.createElement("button"); 
-CoolButton.innerText="button";
 let problems = document.querySelectorAll(".problem-available");
+let AllProblems = document.querySelector(".problems_link");
 
 var names = [];
-var links = []
-for (let i = 0; i < problems.length; i++) {
-    names.push(String(problems[i].querySelector("a").innerHTML))
-    links.push(String(problems[i].querySelector("a").href))
+var links = [];
+var classes = [];
+
+var folder = document.createElement("ul");
+folder.className = "balls";
+AllProblems.appendChild(folder);
+
+
+for (let i = 0; i < problems.length-1; i++) {
+    names.push(problems[i].querySelector("a").innerHTML)
+    links.push(problems[i].querySelector("a").href)
+    classes.push(problems[i].className)
 }
 
-problems[0].appendChild(CoolButton);
-// problems[1].appendChild(CoolButton);
+AddProblem(names, links, classes, folder)
+AddProblem(names, links, classes, folder)
 
-CoolButton.addEventListener("click", clicked);
+for (let i = 0; i < problems.length-1; i++) {
+        problems[i].remove();
+}
 
-function clicked() {
-    alert(names[0]+links[0])
+function AddProblem(names, links, classnames, folder) {
+    for (let i = 0; i < problems.length-1; i++) {
+        let problem = document.createElement("li");
+        problem.className = classnames[i];
+        folder.appendChild(problem);
+
+        let a = document.createElement("a");
+        a.href = links[i];
+        a.innerHTML = names[i];
+        problem.appendChild(a);
+    }
 }
